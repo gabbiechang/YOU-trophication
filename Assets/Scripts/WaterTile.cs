24b221bb93149d1de4b48e0a467abd78a4bc47b3
Gabrielle Chang <gabrielle@journeyfoods.com>
2020-02-01 15:14:48 -0500

    Initial commit

---
+++ Assets/Scripts/WaterTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTile : MonoBehaviour {
+
+    private SpriteRenderer rend;
+    //array of type Sprite
+    public Sprite[] tileGrapics;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+}
